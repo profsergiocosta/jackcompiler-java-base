492b6958017ecd700f8df661d773b944790998ba
Marcelo Anjos <marceloanjos023@gmail.com>
2023-10-01 21:00:53 -0300

    refactor: Recognizing integers

--- src/main/java/br/ufma/ecp/App.java
+++ src/main/java/br/ufma/ecp/App.java
@@ -2,43 +2,17 @@ package br.ufma.ecp;
 
 import static br.ufma.ecp.token.TokenType.*;
 
+import br.ufma.ecp.token.Token;
 
+public class App {
 
-import br.ufma.ecp.token.Token; 
+    public static void main(String[] args) {
 
-public class App 
-{
-
-    
-    public static void main( String[] args )
-    {
-
-    
-        String input = "45  + if - \"ola\" laranja 876";
-        Scanner scan = new Scanner (input.getBytes());
+        String input = "289";
+        Scanner scan = new Scanner(input.getBytes());
+        System.out.println(scan.nextToken());
         for (Token tk = scan.nextToken(); tk.type != EOF; tk = scan.nextToken()) {
             System.out.println(tk);
         }
-
-        /*
-        Parser p = new Parser (input.getBytes());
-        p.parse();
-        */
-
-
-        //Parser p = new Parser (fromFile().getBytes());
-        //p.parse();
-
-        /*
-        String input = "489-85+69";
-        Scanner scan = new Scanner (input.getBytes());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        Token tk = new Token(NUMBER, "42");
-        System.out.println(tk);
-        */
     }
 }
