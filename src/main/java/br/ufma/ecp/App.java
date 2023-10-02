724125ceb96b287ff03dbe03563e9a2f225ab271
Marcelo Anjos <marceloanjos023@gmail.com>
2023-10-01 21:24:04 -0300

    Ignoring comments

--- src/main/java/br/ufma/ecp/App.java
+++ src/main/java/br/ufma/ecp/App.java
@@ -9,19 +9,23 @@ public class App {
     public static void main(String[] args) {
 
         String input = """
-             \"Teste\"
-            int estoque = //valor recuperado do sistema
+                            //comentario de linha
+                            /*
+                            comentario em bloco
+                            */
+                            \"Teste\"
+                            int estoque = //valor recuperado do sistema
 
-            if (estoque >= 100) {
-              estoque = estoque - 100;
-            } else if (estoque < 100 && estoque > 50) {
-                estoque = estoque - 50;
-                } else if (estoque <= 50 && estoque > 0) {
-                estoque = estoque - 10;
-            } else {
-                estoque = 0;
-            }
-                """;
+                            if (estoque >= 100) {
+                            estoque = estoque - 100;
+                            } else if (estoque < 100 && estoque > 50) {
+                                estoque = estoque - 50;
+                                } else if (estoque <= 50 && estoque > 0) {
+                                estoque = estoque - 10;
+                            } else {
+                                estoque = 0;
+                            }
+                        """;
         Scanner scan = new Scanner(input.getBytes());
         for (Token tk = scan.nextToken(); tk.type != EOF; tk = scan.nextToken()) {
             System.out.println(tk);
