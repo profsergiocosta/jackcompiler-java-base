2e421b2ba3158d070f94289804fcac6362bcb135
TonyMPCastro <antonio.m.fg38@outlook.com>
2024-11-21 20:03:12 -0300

    Very Comment

--- src/main/java/br/ufma/ecp/App.java
+++ src/main/java/br/ufma/ecp/App.java
@@ -1,7 +1,5 @@
 package br.ufma.ecp;
 
-import static br.ufma.ecp.token.TokenType.*;
-
 import br.ufma.ecp.token.Token;
 import br.ufma.ecp.token.TokenType;
 
@@ -9,13 +7,30 @@ public class App {
 
     public static void main(String[] args) {
 
-        String input = "45 variavel + while < , if";
+        String input = """
+                // é um comentario 10
+                45 \"hello\" variavel + while < , if
+                /*
+                comentario em bloco
+                */
+                42 ola
+
+                """;
         Scanner scan = new Scanner(input.getBytes());
         for (Token tk = scan.nextToken(); tk.type != TokenType.EOF; tk = scan.nextToken()) {
             System.out.println(tk);
         }
 
-        
+        /*
+         * Teste de Simbolo e Palavras chave
+         * String input = "45 variavel + while < , if";
+         * Scanner scan = new Scanner(input.getBytes());
+         * for (Token tk = scan.nextToken(); tk.type != TokenType.EOF; tk =
+         * scan.nextToken()) {
+         * System.out.println(tk);
+         * }
+         */
+
         /*
          * String input = "45  + if - \"ola\" laranja 876";
          * Scanner scan = new Scanner (input.getBytes());
