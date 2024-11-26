c41ee71b1edbd29949697abfa6193bd5e7e54586
e-Mangueira <nm.mangueira@ufma.br>
2024-11-26 16:35:26 -0300

    correcaoTests

--- src/main/java/br/ufma/ecp/App.java
+++ src/main/java/br/ufma/ecp/App.java
@@ -21,31 +21,5 @@ public class App {
             System.out.println(tk);
         }
 
-        /*
-         * Teste de Simbolo e Palavras chave
-         * String input = "45 variavel + while < , if";
-         * Scanner scan = new Scanner(input.getBytes());
-         * for (Token tk = scan.nextToken(); tk.type != TokenType.EOF; tk =
-         * scan.nextToken()) {
-         * System.out.println(tk);
-         * }
-         */
-
-        /*
-         * String input = "45  + if - \"ola\" laranja 876";
-         * Scanner scan = new Scanner (input.getBytes());
-         * for (Token tk = scan.nextToken(); tk.type != EOF; tk = scan.nextToken()) {
-         * System.out.println(tk);
-         * }
-         */
-
-        /*
-         * Parser p = new Parser (input.getBytes());
-         * p.parse();
-         */
-
-        // Parser p = new Parser (fromFile().getBytes());
-        // p.parse();
-
     }
 }
