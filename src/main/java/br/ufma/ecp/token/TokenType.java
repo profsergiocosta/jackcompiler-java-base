85e882617a8ffc9d842136cf3e77a1909f3611f2
Luan A. <barkon32@tuta.io>
2024-11-27 13:56:50 -0300

    feat: add values to enum in TokenType

--- src/main/java/br/ufma/ecp/token/TokenType.java
+++ src/main/java/br/ufma/ecp/token/TokenType.java
@@ -4,32 +4,41 @@ import java.util.List;
 import java.util.Map;
 
 public enum TokenType {
-    PLUS,MINUS,
+    PLUS("+"),
+    MINUS("-"),
 
      // Literals.
-     NUMBER,
-     STRING,
+     NUMBER("0123456789"),
+     STRING("IDENT"),
 
 
-     IDENT,
+     IDENT("IDENT"),
 
  
      // keywords
-     METHOD,
-     WHILE,
-     IF,
-     CLASS,
-     CONSTRUCTOR,
+     METHOD("method"),
+     WHILE("while"),
+     IF("if"),
+     CLASS("class"),
+     CONSTRUCTOR("constructor"),
 
-     EOF,
+     EOF("\0"),
 
-     ILLEGAL;
+     ILLEGAL("ILLEGAL");
 
      static public boolean isSymbol (char c) {
         String symbols = "{}()[].,;+-*/&|<>=~";
         return symbols.indexOf(c) > -1;
     }
 
+    private String value;
+
+    TokenType(String value) {
+    }
+
+    public String getValue() {
+         return this.value;
+    }
 
     static public boolean isKeyword (TokenType type) {
         List<TokenType> keywords  = 
