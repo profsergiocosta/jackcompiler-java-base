4945c3c247cabbde62671a7d0f2af4d7712e75eb
nervaljunior <nerval.junior@discente.ufma.br>
2024-11-28 17:23:50 -0300

    Line counter created. this include modify in token.java and Scanner java for Scanner test

--- src/main/java/br/ufma/ecp/Scanner.java
+++ src/main/java/br/ufma/ecp/Scanner.java
@@ -41,6 +41,7 @@ public class Scanner {
     private byte[] input;
     private int current;
     private int start;
+    private int line = 1;
 
 
     public Scanner(byte[] input) {
@@ -52,35 +53,49 @@ public class Scanner {
     private void skipWhitespace() {
         char ch = peek();
         while (ch == ' ' || ch == '\r' || ch == '\t' || ch == '\n') {
+
+            if (ch == '\n')
+                line++;
+
             advance();
             ch = peek();
         }
     }
 
     private void skipLineComments() {
-        for (char ch = peek(); ch != '\n' && ch != 0;  advance(), ch = peek()) ;
+
+        for (char ch = peek(); ch != '\n' && ch != 0; advance(), ch = peek())
+            if (ch == '\n')
+                line++;
     }
+
 
     private void skipBlockComments() {
         boolean endComment = false;
         advance();
-        while (!endComment) {
-            advance();
-            char ch = peek();
-            if ( ch == 0) { // eof, lexical error
-                System.exit(1);
-            }
 
-            if (ch == '*') {
-                for (ch = peek(); ch == '*';  advance(), ch = peek()) ;
-                if (ch == '/') {
-                    endComment = true;
-                    advance();
-                }
+        while (!endComment) {
+          advance();
+          char ch = peek();
+
+          if (ch == '\n')
+              line++;
+
+          if (ch == 0) { // eof, lexical error
+            System.exit(1);
+          }
+
+          if (ch == '*') {
+            for (ch = peek(); ch == '*'; advance(), ch = peek())
+              ;
+            if (ch == '/') {
+              endComment = true;
+              advance();
             }
+          }
 
         }
-    }
+      }
 
 
     public Token nextToken() {
@@ -109,70 +124,70 @@ public class Scanner {
                 }
                 else {
                     advance();
-                    return new Token (TokenType.SLASH,"/");
+                    return new Token (TokenType.SLASH,"/", line);
                 }
 
             case '+':
                 advance();
-                return new Token(TokenType.PLUS, "+");
+                return new Token(TokenType.PLUS, "+", line);
             case '-':
                 advance();
-                return new Token(TokenType.MINUS, "-");
+                return new Token(TokenType.MINUS, "-", line);
             case '*':
                 advance();
-                return new Token(TokenType.ASTERISK, "*");
+                return new Token(TokenType.ASTERISK, "*", line);
             case '.':
                 advance();
-                return new Token(TokenType.DOT, ".");
+                return new Token(TokenType.DOT, ".", line);
             case '&':
                 advance();
-                return new Token(TokenType.AND, "&");
+                return new Token(TokenType.AND, "&", line);
             case '|':
                 advance();
-                return new Token(TokenType.OR, "|");
+                return new Token(TokenType.OR, "|", line);
             case '~':
                 advance();
-                return new Token(TokenType.NOT, "~");
+                return new Token(TokenType.NOT, "~", line);
             case '>':
                 advance();
-                return new Token(GREATER, ">");
+                return new Token(GREATER, ">", line);
             case '<':
                 advance();
-                return new Token(LOWER, "<");
+                return new Token(LOWER, "<", line);
             case '=':
                 advance();
-                return new Token(EQUALS, "=");
+                return new Token(EQUALS, "=", line);
             case '(':
                 advance();
-                return new Token(TokenType.LPAREN, "(");
+                return new Token(TokenType.LPAREN, "(", line);
             case ')':
                 advance();
-                return new Token(TokenType.RPAREN, ")");
+                return new Token(TokenType.RPAREN, ")", line);
             case '{':
                 advance();
-                return new Token(TokenType.LBRACE, "{");
+                return new Token(TokenType.LBRACE, "{", line);
             case '}':
                 advance();
-                return new Token(TokenType.RBRACE, "}");
+                return new Token(TokenType.RBRACE, "}", line);
             case '[':
                 advance();
-                return new Token(TokenType.LBRACKET, "[");
+                return new Token(TokenType.LBRACKET, "[", line);
             case ']':
                 advance();
-                return new Token(TokenType.RBRACKET, "]");
+                return new Token(TokenType.RBRACKET, "]", line);
             case ';':
                 advance();
-                return new Token(TokenType.SEMICOLON, ";");
+                return new Token(TokenType.SEMICOLON, ";", line);
             case ',':
                 advance();
-                return new Token(TokenType.COMMA, ",");
+                return new Token(TokenType.COMMA, ",", line);
             case '"':
                 return string();
             case 0:
-                return new Token(EOF, "EOF");
+                return new Token(EOF, "EOF", line);
             default:
                 advance();
-                return new Token(ILLEGAL, Character.toString(ch));
+                return new Token(ILLEGAL, Character.toString(ch), line);
         }
     }
 
@@ -182,7 +197,7 @@ public class Scanner {
         String id = new String(input, start, current - start, StandardCharsets.UTF_8);
         TokenType type = keywords.get(id);
         if (type == null) type = IDENT;
-        return new Token(type, id);
+        return new Token(type, id, line);
     }
 
     private Token number() {
@@ -191,7 +206,7 @@ public class Scanner {
         }
 
         String num = new String(input, start, current - start, StandardCharsets.UTF_8);
-        return new Token(NUMBER, num);
+        return new Token(NUMBER, num, line);
     }
 
     private Token string () {
@@ -201,7 +216,7 @@ public class Scanner {
             advance();
         }
         String s = new String(input, start, current-start, StandardCharsets.UTF_8);
-        Token token = new Token (TokenType.STRING,s);
+        Token token = new Token (TokenType.STRING,s, line);
         advance();
         return token;
     }
