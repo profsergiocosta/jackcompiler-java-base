61483ba9e3e649d6c7b3f464eeba2ef8b3fca250
TonyMPCastro <antonio.m.fg38@outlook.com>
2024-12-05 18:49:30 -0300

    Include new parser

--- src/main/java/br/ufma/ecp/token/Token.java
+++ src/main/java/br/ufma/ecp/token/Token.java
@@ -1,12 +1,10 @@
 package br.ufma.ecp.token;
 
-import static br.ufma.ecp.token.TokenType.isSymbol;
-
 public class Token {
 
     public final TokenType type;
     public final String lexeme;
-    final int line;
+    public final int line;
 
     public Token (TokenType type, String lexeme, int line) {
         this.type = type;
